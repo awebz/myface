95524bec633e27e38b3125f4157260ab3327fc62
azohoori <azohoori@cisco.com>
2015-06-03 16:33:38 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myface::Application.config.secret_key_base = 'b078ac2a2ffc0b8d328392d4d7141a84ef85fb0575501fd59df19a015889b8d472421444ab1f344a0b05d50ef897552172757f214f72bf3749dbcded19cfe73e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Myface::Application.config.session_store :cookie_store, key: '_myface_session'
